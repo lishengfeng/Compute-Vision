591c8474af48d0eae60bc5527a8e5e0326ae9931
Shengfeng Li <cs.shengfeng.li@gmail.com>
2019-02-24 10:27:23 -0600

    Rename folder following the homework order

--- 2-2-Histogram-Equalization-YCRCB/histEql_YCBCR.py
+++ 2-2-Histogram-Equalization-YCRCB/histEql_YCBCR.py
@@ -3,7 +3,8 @@ import numpy as np
 
 
 def myHEQ_YCRCB(img):
-    Y, Cr, Cb = cv2.split(img)
+    imgYCC = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
+    Y, Cr, Cb = cv2.split(imgYCC)
     equ_Y = cv2.equalizeHist(Y)
     equ = cv2.merge((equ_Y, Cr, Cb))
     res = np.hstack((img, equ))
@@ -16,10 +17,9 @@ if __name__ == '__main__':
     clone_image = source_img.copy()
     # source_img = cv2.imread('UnderExposedScene.jpg')
     # clone_image_1 = source_img.copy()
-    imgYCC = cv2.cvtColor(clone_image, cv2.COLOR_BGR2YCR_CB)
 
     while 1:
-        cv2.imshow('HEQ_YCRCB', myHEQ_YCRCB(imgYCC))
+        cv2.imshow('HEQ_YCRCB', myHEQ_YCRCB(clone_image))
         # cv2.imshow('HEQ_RGB_1', myHEQ_RGB(clone_image_1))
         k = cv2.waitKey(20) & 0xFF
         if k == 27:  # 27 -> ESC
